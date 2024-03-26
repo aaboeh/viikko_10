e35fcd59416a3152c951f22256abf1cd4c749060
aaboeh <aapo.aarvala@gmail.com>
2024-03-26 15:24:50 +0200

    Third Commit

--- app/src/main/java/com/example/viikko10/ListUserActivity.java
+++ app/src/main/java/com/example/viikko10/ListUserActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class ListUserActivity extends AppCompatActivity {
 
     private UserStorage ustorage;
@@ -17,10 +21,16 @@ public class ListUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_user_in_recycle_view);
 
         ustorage = UserStorage.getInstance();
+        ArrayList<User> users = ustorage.getUsers();
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                return user1.getLastName().compareTo(user2.getLastName());
+            }
+        });
 
         recyclerView = findViewById(R.id.rvUserView);
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new UserListAdapter(getApplicationContext(), ustorage.getUsers()));
+        recyclerView.setAdapter(new UserListAdapter(getApplicationContext(), users));
     }
 }
