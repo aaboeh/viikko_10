9aac71400ef3c508d55d552223d24b4a50580648
aaboeh <aapo.aarvala@gmail.com>
2024-03-27 16:33:13 +0200

    sixth Commit

--- app/src/main/java/com/example/viikko10/MainActivity.java
+++ app/src/main/java/com/example/viikko10/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = MainActivity.this;
-        loadUsers();
+        UserStorage.getInstance().loadUsers(context);
     }
 
     public void switchToAddUser(View view) {
